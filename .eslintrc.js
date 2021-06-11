a28dd00149f4738a961ae853d1487ddf1ad5581f
shreyas <spapinwar@gmail.com>
2021-06-11 09:15:07 +0530

    Create react web and setup ci travis

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+//Path: /.eslintrc.js
+
+module.exports = {
+    extends: ['airbnb'],
+    rules: {
+      'react/jsx-filename-extension': 'off',
+      'array-bracket-spacing': ['error', 'never'],
+      'no-use-before-define': ['warn', { functions: true, classes: true }],
+      'no-param-reassign': ['warn'],
+      'no-use-before-define': 'off',
+      'no-param-reassign': 'off',
+      //You can override any rules you want
+    },
+    env: {
+      browser: true,
+      jest: true,
+    },
+  };
