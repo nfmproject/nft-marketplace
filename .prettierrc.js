67320077947cc17ed13f2caa4f7bd1926f265d5a
shreyas <spapinwar@gmail.com>
2021-06-11 09:23:33 +0530

    Updated as per eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 //Path: /.prettierrc.js
 
 module.exports = {
-    bracketSpacing: true,
-    jsxBracketSameLine: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    // Override any other rules you want
-  };
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  // Override any other rules you want
+};
